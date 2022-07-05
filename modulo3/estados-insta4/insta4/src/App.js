d3d67dc5eed1eae50152c267004e4b085ee7c1a1
Rhuan Victor Virtudes Lourenco <rhvirtudes1@gmail.com>
2022-07-05 20:25:33 -0300

    exercicio-renderizacao-de-listas

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -1,36 +1,44 @@
 import React from 'react';
-import styled from 'styled-components'
 import Post from './components/Post/Post';
+import styled from 'styled-components';
 
-const MainContainer = styled.div`
+export const Main = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
   align-items: center;
 `
+
 function App() {
+  const usuarios = [
+    {
+        nomeUsuario: "paulinha",
+        fotoUsuario: "https://picsum.photos/50/50",
+        fotoPost: 'https://picsum.photos/200/150'
+    },
+    {
+        nomeUsuario: "astrodev",
+        fotoUsuario: "https://thepracticaldev.s3.amazonaws.com/i/6hqmcjaxbgbon8ydw93z.png",
+        fotoPost: "https://pbs.twimg.com/profile_images/1389789795065335809/A8H1fuQb_400x400.jpg"
+    },
+    {
+        nomeUsuario: "astrobot",
+        fotoUsuario: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Gnome-stock_person_bot.svg/2048px-Gnome-stock_person_bot.svg.png",
+        fotoPost: "https://i0.wp.com/www.opuspesquisa.com/wp-content/uploads/2021/03/Sistema-bot-1.jpg?resize=992%2C558&ssl=1"
+    }
+]
+
+const postUsuario = usuarios.map((pessoa, index) => {
+    return (
+        <Post key={index} fotoPost={pessoa.fotoPost} fotoUsuario={pessoa.fotoUsuario} nomeUsuario={pessoa.nomeUsuario} />
+    )
+})
+
 return(
-  <MainContainer>
-           <Post
-            nomeUsuario={'paulinha'}
-            fotoUsuario={'https://picsum.photos/50/50'}
-            fotoPost={'https://picsum.photos/200/150'}
-          />
-           <Post
-            nomeUsuario={'astrodev'}
-            fotoUsuario={'https://thepracticaldev.s3.amazonaws.com/i/6hqmcjaxbgbon8ydw93z.png'}
-            fotoPost={'https://pbs.twimg.com/profile_images/1389789795065335809/A8H1fuQb_400x400.jpg'}
-          />
-            <Post
-            nomeUsuario={'astrobot'}
-            fotoUsuario={'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Gnome-stock_person_bot.svg/2048px-Gnome-stock_person_bot.svg.png'}
-            fotoPost={'https://i0.wp.com/www.opuspesquisa.com/wp-content/uploads/2021/03/Sistema-bot-1.jpg?resize=992%2C558&ssl=1'}
-          />
-
-        </MainContainer>
+  <Main>
+    {postUsuario}
+  </Main>
 )
-
 }
-
 
 export default App;
