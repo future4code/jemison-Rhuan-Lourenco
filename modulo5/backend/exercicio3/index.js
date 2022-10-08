3769a436376f8b3b5c9a29d78cfade716eb08fcc
Rhuan Victor Virtudes Lourenco <rhvirtudes1@gmail.com>
2022-10-07 23:30:53 -0300

    resolucao dos exercicios

--- modulo5/backend/exercicio3/index.js
+++ modulo5/backend/exercicio3/index.js
@@ -1,1 +1,24 @@
-console.log("exercicio 3");
+const contas = [
+	{
+		email: `astrodev@labenu.com`,
+		password: `abc123`
+	},
+	{
+		email: `bananinha@gmail.com`,
+		password: `bananinha`
+	}
+]
+
+const validation = (email, password) => {
+	const confirmation = contas.find((element) => {
+		return element.email === email && element.password === password
+	},console.log(confirmation))
+
+	if (confirmation === true) {
+		return (`login bem-sucedido`)
+	} else {
+		return (`e-mail ou senha incorretos`)
+	}
+}
+
+console.log(validation(`astrodev@labenu.com`, `abc123`))
--- modulo5/backend/exercicio4/exercicio.js
+++ modulo5/backend/exercicio4/exercicio.js
@@ -1,1 +1,25 @@
-console.log("exercicio 4");
+const primeiraLista = [
+	{
+		nome: "Banana"
+	},
+	{
+		nome: "Laranja"
+	}
+]
+
+const segundaLista= [
+	{
+		nome: "Laranja"
+	},
+	{
+		nome: "Cebola"
+	}
+]
+
+const terceiraLista = [...primeiraLista, ...segundaLista]
+
+const filteredArray = terceiraLista.filter(function(current, next){
+    return terceiraLista.findIndex(item => item.nome === current.nome) == next
+}) 
+
+console.log(filteredArray);
